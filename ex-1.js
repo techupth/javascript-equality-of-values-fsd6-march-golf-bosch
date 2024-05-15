f7c3e5bcedfa651a27153f564f4e7abccb4e9495
Kunakorn <kunakorn.ph98@gmail.com>
2024-05-15 13:00:44 +0700

    Assignment#1 Equality March

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,20 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
-Object.is(0, -0);
-Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
-Object.is(null, undefined);
+Object.is(10, 20); //False
+Object.is(0, -0); //True
+Object.is(NaN, 100); //False
+Object.is("one", "two"); //False
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //True => False (referance value of object when compare is address in memory but each object store value in differance address that why the result is False)
+Object.is(null, undefined); //False
 
-10 === 20;
-10 === "10"
-3.14 !== 3.14;
-"TechUp" !== "TechUp";
-NaN === NaN;
-0 !== -0;
+10 === 20; //False
+10 === "10" //False
+3.14 !== 3.14; ///False
+"TechUp" !== "TechUp"; //False
+NaN === NaN; //False
+0 !== -0; // False (Af)
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;
+objectA === objectB; //True = False (referance value of object when compare is address in memory but each object store value in differance address that why the result is False)
+
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,8 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //True
+null == NaN; //false => True (The end result is same as "blank" so loose equality logic seem to see it as equal)
+10 == "10";  //True
+[10] == 10;  //False => True (The end result is same so loose equality logic seem to see it as equal)
+[1,2] == "1,2"; //false => True (The end result is same so loose equality logic seem to see it as equal)
+
